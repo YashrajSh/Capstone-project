1f727d73c35e653b89d2688916e47235538191e3
YashrajSh <yashraj.shri4@gmail.com>
2026-04-08 10:00:04 +0530

    Code Changes Implemented

--- 7th April/SIT_202.java
+++ 7th April/SIT_202.java
@@ -23,14 +23,19 @@ public class SIT_202 {
 
         head.next.next.next.next.next = head.next; // creating a cycle..Node 50 points to the node 20
         
-        //boolean hasCycle = detectCycle(head);
+        boolean hasCycle = detectCycle(head);
+        if (hasCycle) {
+            System.out.println("Cycle removed. The linked list after removal is:");
+        } else {
+            System.out.println("No cycle found. The linked list is:");
+        }
         printList(head);
-
     }
+
     static boolean detectCycle(Node_SIT head){
         Node_SIT slow = head;
         Node_SIT fast = head;
-        while(fast!=null && fast.next!=null){
+        while(fast != null && fast.next != null){
             slow = slow.next; // move slow by 1 step
             fast = fast.next.next; // move fast by 2 steps
 
@@ -40,14 +45,30 @@ public class SIT_202 {
                 return true;
             }
         }
+        return false;
+    }
 
-        
+    static void removeCycle(Node_SIT meetingPoint, Node_SIT head){
+        Node_SIT ptr1 = head;
+        Node_SIT ptr2 = meetingPoint;
 
+        // Find the start of the cycle
+        while(ptr1 != ptr2){
+            ptr1 = ptr1.next;
+            ptr2 = ptr2.next;
+        }
+
+        // ptr1 (or ptr2) is now at the start of the cycle
+        Node_SIT prev = ptr1;
+        while(prev.next != ptr1){
+            prev = prev.next;
+        }
+        prev.next = null; // break the cycle
     }
 
     static void printList(Node_SIT head){
         Node_SIT current = head;
-        while(current!=null){
+        while(current != null){
             System.out.print(current.data + " ");
             current = current.next;
         }
--- 7th April/SIT_203.java
+++ 7th April/SIT_203.java
@@ -1,87 +1,97 @@
 import java.util.*;
-class Order{
+
+class Order {
     int orderID;
     String customerType;
     double orderValue;
     int deliveryTime; // in hours
+
+    Order(int orderID, String customerType, double orderValue, int deliveryTime) {
+        this.orderID = orderID;
+        this.customerType = customerType;
+        this.orderValue = orderValue;
+        this.deliveryTime = deliveryTime;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("OrderID: %d, CustomerType: %s, OrderValue: %.2f, DeliveryTime: %dh",
+                orderID, customerType, orderValue, deliveryTime);
+    }
 }
 
-Order(int orderID, String customerType, double orderValue, int deliveryTime){
-    this.orderID = orderID;
-    this.customerType = customerType;
-    this.orderValue = orderValue;
-    this.deliveryTime = deliveryTime;
-}
-
-public class SIT_203{
+public class SIT_203 {
     public static void main(String[] args) {
         Order[] orders = {
-            new Order(101, "Regular", 500.0, 5),
-            new Order(102, "Prime", 300.0, 3),
-            new Order(103, "Prime", 700.0, 4),
-            new Order(104, "Regular", 500.0, 2),
-            new Order(105, "Prime", 700.0, 2)
+                new Order(101, "Regular", 500.0, 5),
+                new Order(102, "Prime", 300.0, 3),
+                new Order(103, "Prime", 700.0, 4),
+                new Order(104, "Regular", 500.0, 2),
+                new Order(105, "Prime", 700.0, 2)
         };
-        mergeSort(orders,0,orders.length-1);
 
+        mergeSort(orders, 0, orders.length - 1);
+        System.out.println("Sorted orders:");
+        printOrders(orders);
     }
-    static void mergeSort(Order[] orders, int left, int right){
-        if(left<right){
-            int mid = left + (right-left)/2;
-            mergeSort(orders,left,mid);
-            mergeSort(orders,mid+1,right);
-            merge(orders,left,mid,right);
+
+    static void mergeSort(Order[] orders, int left, int right) {
+        if (left < right) {
+            int mid = left + (right - left) / 2;
+            mergeSort(orders, left, mid);
+            mergeSort(orders, mid + 1, right);
+            merge(orders, left, mid, right);
         }
     }
 
-    static void merge(Order[] orders, int left, int mid, int right){
+    static void merge(Order[] orders, int left, int mid, int right) {
         int n1 = mid - left + 1;
         int n2 = right - mid;
 
         Order[] leftArr = new Order[n1];
         Order[] rightArr = new Order[n2];
 
-        for(int i=0;i<n1;i++){
-            leftArr[i] = orders[left+i];
+        for (int i = 0; i < n1; i++) {
+            leftArr[i] = orders[left + i];
         }
-        for(int j=0;j<n2;j++){
-            rightArr[j] = orders[mid+1+j];
+        for (int j = 0; j < n2; j++) {
+            rightArr[j] = orders[mid + 1 + j];
         }
 
-        int i=0,j=0,k=left;
-        while(i<n1 && j<n2){
-            if(leftArr[i].orderValue > rightArr[j].orderValue){
+        int i = 0, j = 0, k = left;
+        while (i < n1 && j < n2) {
+            if (leftArr[i].orderValue > rightArr[j].orderValue) {
                 orders[k++] = leftArr[i++];
-            }
-            else if(leftArr[i].orderValue < rightArr[j].orderValue){
+            } else if (leftArr[i].orderValue < rightArr[j].orderValue) {
                 orders[k++] = rightArr[j++];
-            }
-            else{
-                if(leftArr[i].deliveryTime < rightArr[j].deliveryTime){
+            } else {
+                if (leftArr[i].deliveryTime < rightArr[j].deliveryTime) {
                     orders[k++] = leftArr[i++];
-                }
-                else if(leftArr[i].deliveryTime > rightArr[j].deliveryTime){
+                } else if (leftArr[i].deliveryTime > rightArr[j].deliveryTime) {
                     orders[k++] = rightArr[j++];
-                }
-                else{
-                    if(leftArr[i].customerType.equals("Prime") && rightArr[j].customerType.equals("Regular")){
+                } else {
+                    if (leftArr[i].customerType.equals("Prime") && rightArr[j].customerType.equals("Regular")) {
                         orders[k++] = leftArr[i++];
-                    }
-                    else if(leftArr[i].customerType.equals("Regular") && rightArr[j].customerType.equals("Prime")){
+                    } else if (leftArr[i].customerType.equals("Regular") && rightArr[j].customerType.equals("Prime")) {
                         orders[k++] = rightArr[j++];
-                    }
-                    else{
+                    } else {
                         orders[k++] = leftArr[i++];
                     }
                 }
             }
         }
 
-        while(i<n1){
+        while (i < n1) {
             orders[k++] = leftArr[i++];
         }
-        while(j<n2){
+        while (j < n2) {
             orders[k++] = rightArr[j++];
+        }
+    }
+
+    static void printOrders(Order[] orders) {
+        for (Order order : orders) {
+            System.out.println(order);
         }
     }
 }
